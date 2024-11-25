a68946842cbf5e7a75d5940d892e645ecdc059c2
Honey bansal <honeybansal2968@gmail.com>
2024-11-25 20:17:03 +0530

    dfd

--- api/index.py
+++ api/index.py
@@ -56,5 +56,5 @@ def fetch_title():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.20", port=7860, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.20", port=7860, debug=True)
